4e98303df4fb628514514bf10a4d597e4d1e1546
Brig1tte <korantengbrigitte96@gmail.com>
2023-10-17 22:55:50 -0700

    MySQL Project

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,5 @@
+--A Script to list all rows of the first_table from the database hbtn_0c_0 in MySQL server
+-- All fields should be printed
+-- Database name is passed as an arg of the mysql command
+
+SELECT * FROM first_table;
---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,7 @@
+--A Script to insert a new row in the first_table (database hbtn_0c_0) in MySQL server
+-- New row:
+--     id = 89
+--     name = Best school
+-- database name is passed as an arg of mysql command
+
+INSERT INTO `first_table` (`id`, `name`) VALUES(89, 'Best School');
