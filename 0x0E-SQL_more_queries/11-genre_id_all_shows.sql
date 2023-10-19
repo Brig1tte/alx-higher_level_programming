6fe14f92617cbe6f3c81b3dcd87e9c7e602f33f4
Brig1tte <korantengbrigitte96@gmail.com>
2023-10-18 17:01:28 -0700

    More SQL Queries

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,9 @@
+-- The script to list all shows contained in the database hbtn_0d_tvshows
+-- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+-- To display NULL for shows that do not have genres
+
+SELECT s.`title`, g.`genre_id`
+  FROM `tv_shows` AS s
+       LEFT JOIN `tv_show_genres` AS g
+       ON s.`id` = g.`show_id`
+ ORDER BY s.`title`, g.`genre_id`;
