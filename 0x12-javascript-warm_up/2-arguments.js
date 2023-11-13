814936149aa53b12339650b208daf58c3a1afb96
Brig1tte <korantengbrigitte96@gmail.com>
2023-11-13 11:02:46 -0800

    My_First_JavaScript

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,13 +3,10 @@
 // messages: "No argument", "Argument found"
 
 const argsNum = process.argv.slice(2).length;
-if (argsNum === 0)
-{
-	console.log('No argument');
-} else if (argsNum === 1)
-{
-	console.log('Argument found');
-} else
-{
-	console.log('Arguments found');
+if (argsNum === 0) {
+	console.log('No argument')
+} else if (argsNum === 1) {
+	console.log('Argument found')
+} else {
+	console.log('Arguments found')
 }
