b4f84514921f469e9c931d4884c60dcf7a4600ff
Brig1tte <korantengbrigitte96@gmail.com>
2023-11-13 10:55:02 -0800

    My_First_JavaScript

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// a script to print a message dependent on numb of args passed:
+// messages: "No argument", "Argument found"
+
+const argsNum = process.argv.slice(2).length;
+if (argsNum == 0)
+{
+	console.log('No argument');
+} else if (argsNum == 1)
+{
+	console.log('Argument found');
+} else
+{
+	console.log('Arguments found');
+}
