a64c112075b98f652a42aa15299f691e3d78620b
Brig1tte <korantengbrigitte96@gmail.com>
2023-11-13 18:19:20 -0800

    My_First_JavaScript

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+// a script to compute and print a factorial
+let num = process.argv[2];
+
+if (!num) {
+  num = 0;
+}
+
+function factorial (num) {
+	if (num === 0) {
+		return (1);
+	} else {
+		return (num * factorial(num - 1));
+	}
+}
+
+console.log(factorial(num));
---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// a script to search the second biggest integer in the list of arguments
+const args = process.argv.slice(2);
+function compare (a, b) {
+        return a - b;
+}
+if (args.length === 0) {
+        console.log('0');
+} else if (args.length === 1) {
+        console.log('0');
+} else {
+        console.log(args.sort(compare)[args.length - 2]);
+}
