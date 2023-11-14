a73a5664713da977cca7d1bb18864418f7b4fe75
Brig1tte <korantengbrigitte96@gmail.com>
2023-11-13 17:12:21 -0800

    My_First_JavaScript

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // a script to print a message dependent on numb of args passed:
-const argsNum = process.argv.slice(2).length;
+const argsNum = process.argv.length - 2;
 if (argsNum === 0) {
 	console.log('No argument');
 } else if (argsNum === 1) {
