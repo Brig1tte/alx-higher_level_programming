332d2517a16c8ad423f7f14a7a888f94590c33c5
Brig1tte <korantengbrigitte96@gmail.com>
2023-11-17 22:04:17 -0800

    JavaScript Warm Up

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,5 +2,5 @@
 // a script to print 3 lines by using an array of string and a loop
 const lines = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 for (let j = 0; j < lines.length; j++) {
-  console.log(`${lines[j]}`);
+  console.log(lines[j]);
 }
