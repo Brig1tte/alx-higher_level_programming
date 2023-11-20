cfa68a9ab17dc9a05e2da41a2409677509d75219
Brig1tte <korantengbrigitte96@gmail.com>
2023-11-20 09:14:44 -0800

    Update

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" A script to list all cities of the database hbtn_0e_4_usa, ordered by city id """
+""" A script to list all cities of the database hbtn_0e_4_usa """
 import sys
 import MySQLdb
 
