732394f9febce84fa5e8d7fd03ddc137f626cfdb
Brig1tte <korantengbrigitte96@gmail.com>
2023-11-27 15:19:57 -0800

    Creating and using Python ORM

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -9,7 +9,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-class State(Base):
+ class State(Base):
     """This class State represents a state for a MySQL database
     __tablename__ (str): The name of the MySQL table to store States.
     id (sqlalchemy.Integer): The state's id.
